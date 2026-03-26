52fdddfd072dcb72107c98190a491f62f0bd4beb
anutaluzhetska <anyluzeckaana.study@gmail.com>
2026-03-26 12:52:28 +0200

    connection settings and automatic creation of PostgreSQL tables

--- db/connector.js
+++ db/connector.js
@@ -8,5 +8,35 @@ const pool = new Pool({
 });
 
 //Add table creation
+const createTableQueries = [];
+
+createTableQueries.push(`
+    CREATE TABLE IF NOT EXISTS heroes1 (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,              
+    primary_attribute TEXT,        
+    role TEXT,       
+    attack_type TEXT,           
+    difficulty INTEGER,                
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+   `);
+createTableQueries.push(`
+ CREATE TABLE IF NOT EXISTS sloniki1 (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    age TEXT NOT NULL,
+    place_of_birth TEXT NOT NULL,           
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   );
+      `);
+
+for (let i = 0; i < createTableQueries.length; i++) {
+    console.log(`create string`)
+    pool.query(createTableQueries[i]);
+}
+
+console.log("CONNECTED!!!!!✅ ")
 
 export default pool;
