08e3629c9fea6fd94ca21387319ed0bcdb0c97a5
PokotyloTaras <taras.h.pokotylo@ukd.edu.ua>
2026-04-27 10:03:02 +0300

    Update dead_space.js

--- routes/dead_space.js
+++ routes/dead_space.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 import db from '../db/connector.js';
 
-// Class 
+// Class Validation
 // -------------------------------------------------------------
 class ValidationError extends Error {
   constructor(message) {
